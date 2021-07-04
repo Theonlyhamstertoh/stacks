8d9fb148e8d22638e92a51b55c98738b322036c7
Theonlyhamstertoh <theonlyhamstertoh@gmail.com>
2021-07-04 12:20:35 -0700

    camera following the top cube

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import * as THREE from "three";
-import { Canvas, useFrame } from "@react-three/fiber";
-import { useTexture, OrbitControls, Html } from "@react-three/drei";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import { useTexture, OrbitControls, Html, OrthographicCamera, useHelper } from "@react-three/drei";
 import { useRef, useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
+
+const CreateBlock = ({ position, color, direction, move, size }) => {
+  return (
+    <mesh position={position}>
+      <boxBufferGeometry args={size} />
+      <meshLambertMaterial color={color} />
+    </mesh>
+  );
+};
+
 export default function App() {
   const [stacks, setStacks] = useState([]);
 
@@ -17,6 +27,7 @@ export default function App() {
     createNewLayer();
   };
 
+  const moveCameraUp = ({ position }) => {};
   const createNewLayer = () => {
     const color = `hsl(${180 + stacks.length * 2},  100%, 60%)`;
     const position = [0, stacks.length, 0];
@@ -31,40 +42,41 @@ export default function App() {
       move: true,
     };
     setStacks((prev) => [...prev, newBlock]);
-    // useEffect(() => {
-    //   console.log(ref);
-    // }, []);
-
-    // return (
-    //   <mesh ref={ref} {...props}>
-    //     <boxBufferGeometry />
-    //     <meshLambertMaterial />
-    //   </mesh>
-    // );
   };
 
   return (
     <div className="fullScreen" onClick={handleClick} onKeyDown={handleKey} tabIndex={-1}>
-      <Canvas orthographic camera={{ zoom: 100, position: [100, 50, 100] }}>
-        <OrbitControls enablePan={false} rotateSpeed={1} />
+      <Canvas>
+        <Camera stacks={stacks} />
         <color attach="background" args={["#222"]} />
         <ambientLight args={[0xffffff, 0.4]} />
         <directionalLight args={[0xffffff, 0.6]} position={[50, 100, 50]} />
 
         {/* Block */}
         {stacks.map((blockInfo) => {
-          return <CreateBlock {...blockInfo} />;
+          moveCameraUp(blockInfo.position);
+          return <CreateBlock {...blockInfo} key={blockInfo.key} />;
         })}
       </Canvas>
     </div>
   );
 }
 
-const CreateBlock = ({ position, color, direction, move, size }) => {
-  return (
-    <mesh position={position}>
-      <boxBufferGeometry args={size} />
-      <meshLambertMaterial color={color} />
-    </mesh>
-  );
+const Camera = ({ stacks }) => {
+  const ref = useRef();
+  const { camera } = useThree();
+  const speed = 0.15;
+  useFrame(({ camera, clock }) => {
+    console.log(stacks.length, camera.position.y - 120);
+    if (stacks.length > camera.position.y - 120) {
+      camera.position.y += speed;
+    }
+    // console.log(stacks.length - 1, camera.position.y - 50);
+  });
+
+  camera.lookAt(new THREE.Vector3(0, stacks.length, 0));
+  camera.updateProjectionMatrix();
+  // useHelper(ref, THREE.CameraHelper, 1, "hotpink");
+
+  return <OrthographicCamera ref={ref} makeDefault position={[100, 120, 100]} zoom={75} />;
 };
